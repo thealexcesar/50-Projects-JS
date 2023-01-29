c3fdbf0c46cb469075c6c55bdf28d5d337c3ae51
thealexcesar <thealexcesar@gmail.com>
2023-01-29 20:47:52 -0300

    Progress Bar

--- Day1/script.js
+++ Day1/script.js
@@ -1,27 +1,47 @@
+const actives = document.querySelectorAll('.active');
+const circles  = document.querySelectorAll('.circle');
+const prev     = document.getElementById('prev');
 const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
-
+const next     = document.getElementById('next');
 let currentActive = 1;
+progress.style.width = currentActive + "%";
+
+toogleButtons(prev, true);
+toogleButtons(next);
+circles.forEach((element, index) => {
+    element.addEventListener('click', function() {
+        document.querySelector('.active').classList.remove('active');
+        element.classList.remove('inactive');
+        element.classList.add('active');
+        currentActive = (index + 1) / 4 * 100;
+        progress.style.width = currentActive <= 100 && currentActive + "%";
+        toogleButtons(prev, true);
+        toogleButtons(next);
+    });
+});
 next.addEventListener('click', () => {
     currentActive++;
-    currentActive = currentActive > circles.length && circles.length;
+    currentActive += currentActive > circles.length && circles.length;
     update();
 });
 
 prev.addEventListener('click', () => {
     currentActive--;
-    currentActive = currentActive < 1 && circles.length;
+    currentActive += currentActive < 1 && 1;
     update();
 });
 
 function update() {
     circles.forEach((circle, index) => {
-        index < currentActive ? circle.classList.add('active') : circle.classList.remove('active')
-        console.log(index);
+        index < currentActive ? circle.classList.add('active') : circle.classList.remove('active');
+        console.log(progress.style.width = currentActive + "%");
     });
+    toogleButtons(prev, true);
+    toogleButtons(next);
+    progress.style.width = currentActive <= 4 ? (currentActive / 4) * 100 + '%' : '100%';
+}
 
-    const actives = document.querySelectorAll('.active');
-    console.log(actives.length / circles.length);
+function toogleButtons(btn, prev=false) {
+    btn.disabled = prev ? (currentActive === 1 ? true : false) : (currentActive === circles.length ? true : false);
+    btn.classList.toggle('disabled', btn.disabled);
 }
